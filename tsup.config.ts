537caba36523e383f807cd8b6bb807649449ddff
tsuno08 <nozomutsu.0806@gmail.com>
2026-02-06 23:58:51 +0900

    build: tsupの設定を最適化してバンドルサイズを削減

---
+++ tsup.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from "tsup";
+
+export default defineConfig({
+  entry: ["src/index.ts"],
+  format: ["cjs", "esm"],
+  dts: true,
+  clean: true,
+  minify: true,
+  outExtension({ format }) {
+    return {
+      js: format === "esm" ? ".js" : ".cjs",
+    };
+  },
+  esbuildOptions(options) {
+    options.charset = "utf8";
+  },
+});
