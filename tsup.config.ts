32ca0428279bf13c0184a70c1cf485303a829253
tsuno08 <nozomutsu.0806@gmail.com>
2026-02-07 00:36:19 +0900

    build: ソースマップを生成しつつ npm パッケージからは除外するよう改善

--- tsup.config.ts
+++ tsup.config.ts
@@ -4,7 +4,7 @@ export default defineConfig({
   entry: ["src/index.ts"],
   format: ["cjs", "esm"],
   dts: true,
-  sourcemap: false,
+  sourcemap: true,
   clean: true,
   minify: true,
   outExtension({ format }) {
